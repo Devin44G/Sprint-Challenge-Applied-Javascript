03ac7b1684a759748f9fee40e5bf76e7844ac624
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-01-17 11:34:05 -0600

    Tabs.js completed

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,19 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+console.log(axios.get('https://lambda-times-backend.herokuapp.com/topics'));
+
+const TOPICS = document.querySelector('.topics');
+
+function tab(obj) {
+  obj.topics.map(topic => {
+    const TAB = document.createElement('div');
+          TAB.classList.add('tab');
+          TAB.textContent = topic;
+          TOPICS.append(TAB);
+  });
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+      .then( res => tab(res.data) );
